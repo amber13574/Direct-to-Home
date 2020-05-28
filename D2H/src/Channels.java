638535c2002c9329a18f9faad2ca5ab37b1b1050
Amber Pandey <amber13574@gmail.com>
2020-05-29 00:29:30 +0530

    Update Channels.java

--- D2H/src/Channels.java
+++ D2H/src/Channels.java
@@ -1,8 +1,8 @@
 public enum Channels {
 
     Zee("Zee", 10), Sony("Sony", 15), StarPlus("Star Plus", 20), Discovery("Discovery", 10), NatGeo("Nat Geo", 20);
+    
     private int amount;
-
     private String name;
 
     Channels(String name, int amount) {
